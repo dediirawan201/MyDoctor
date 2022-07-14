6b54b2601923bf4cc1fcc517c9fc5f9a3399bb53
dediirawan201 <dediirwansyah1925@gmail.com>
2022-07-14 11:36:45 +0700

    membuat tab hospitals

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -5,5 +5,7 @@ import HomeProfile from "./HomeProfile";
 import NewsItem from "./NewsItem";
 import RatedDoctor from "./RatedDoctor";
 import ListDoctor from "./ListDoctor";
+import ListHospitals from "./ListHospitals";
+
 export {Header,BottomNavigator,DoctorCategory,HomeProfile,
-    NewsItem, RatedDoctor, ListDoctor}
+    NewsItem, RatedDoctor, ListDoctor, ListHospitals}
--- src/pages/Hospitals/index.js
+++ src/pages/Hospitals/index.js
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {Gap, ListHospitals} from '../../components';
+import { ILHospitalBG } from '../../assets';
+import { colors, fonts } from '../../utils';
 
 const Hospitals = () => {
   return (
-    <View>
-      <Text>Hospitals</Text>
+    <View style={styles.container}>
+      <ImageBackground source={ILHospitalBG} style={styles.background}>
+        <Text style={styles.title}>Nearby Hospital</Text>
+        <Text style={styles.slot}>3 Tersedia</Text>
+      </ImageBackground>
+      <View style={styles.content}>
+        <ListHospitals/>
+        <ListHospitals/>
+        <ListHospitals/>
+      </View>
     </View>
   )
 }
 
 export default Hospitals;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:colors.secondary,
+    flex:1,
+  },
+  content:{
+    backgroundColor:colors.white,
+    borderRadius:20,
+    flex:1,
+    marginTop:-16,
+    paddingTop:14
+    
+
+  },
+  background:{
+    height:240
+  },
+  title:{
+    paddingTop:30,
+    fontFamily:fonts.primary[600],
+    fontSize:20,
+    color:colors.white,
+    textAlign:'center'
+  },
+  slot:{
+    fontFamily:fonts.primary[300],
+    fontSize:12,
+    textAlign:'center',
+    color:colors.white
+  }
+})
